6b1e371a3a92de9778020ba753a381b571e5b616
HadiqaGohar <160311280+HadiqaGohar@users.noreply.github.com>
2024-03-22 08:38:04 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+"use strict";
+//COMPARATION OPERATOR
+// Practice Question 1
+let val1 = 5;
+let val2 = 10;
+console.log(val1 > val2); //false
+// Practice Question 2
+let val3 = 40;
+let val4 = 50;
+console.log(val3 < val4); //true
+//Practice Question 3
+let num1 = 50;
+let num2 = 20;
+console.log(num1 != num2); //true
+//Practice Question 4
+let firstName = "Hadiqa";
+console.log(firstName == 'Hadiqa' && firstName.length == 5);
+//            true  && false  =  false
+//Practice Question 5
+let fruit = "Mango";
+let vege = "Potato";
+console.log(fruit == vege); //false
+//Practice Question 6
+let age1 = 18;
+let age2 = 20;
+console.log(age1 <= age2); // true
+//Practice Question 7
+let rollNum1 = 12;
+let rollNum2 = 9;
+console.log(rollNum1 >= rollNum2); //true
